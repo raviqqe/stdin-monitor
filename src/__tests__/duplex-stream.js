ff63d9bb076668821490cb1e6055b3046faa3646
Yota Toyama <raviqqe@gmail.com>
2018-07-24 17:20:46 -0700

    Implement write method of duplex stream

--- src/__tests__/duplex-stream.js
+++ src/__tests__/duplex-stream.js
@@ -11,3 +11,10 @@ test("Create a duplex stream", () => {
 test("Check if a stream is a duplex stream", () => {
 	expect(new DuplexStream()).toBeInstanceOf(Duplex);
 });
+
+test("Write to a duplex stream", () => {
+	const stream = new DuplexStream();
+
+	stream.write("foo");
+	stream.write("bar");
+});
--- src/duplex-stream.js
+++ src/duplex-stream.js
@@ -2,4 +2,24 @@ import {
 	Duplex
 } from "stream";
 
-export default class extends Duplex {}
+export default class extends Duplex {
+	constructor(options) {
+		super(options);
+
+		this.lastFragment = "";
+		this.lines = [];
+	}
+
+	_write(chunk, encoding, callback) {
+		if (Buffer.isBuffer(chunk)) {
+			chunk = chunk.toString();
+		}
+
+		const lines = chunk.split("\n");
+
+		this.lastFragment += lines.pop();
+		this.lines.push(...lines);
+
+		callback();
+	}
+}
