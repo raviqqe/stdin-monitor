d1589d7a785bc6687d180fa39c89a602621919dc
Yota Toyama <raviqqe@gmail.com>
2018-07-24 17:24:17 -0700

    Rename duplex stream as monitor stream

---
+++ src/__tests__/monitor-stream.js
@@ -0,0 +1,20 @@
+import {
+	Duplex
+} from "stream";
+
+import MonitorStream from "../monitor-stream";
+
+test("Create a monitor stream", () => {
+	const _ = new MonitorStream();
+});
+
+test("Check if a monitor stream is a duplex stream", () => {
+	expect(new MonitorStream()).toBeInstanceOf(Duplex);
+});
+
+test("Write to a monitor stream", () => {
+	const stream = new MonitorStream();
+
+	stream.write("foo");
+	stream.write("bar");
+});
